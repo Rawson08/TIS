11b64fb9455f1cfa76e088b8d451b27d4d78d928
Aayush Kafle <akafle1@unm.edu>
2023-03-29 20:57:32 -0600

    add input for the Parser

--- src/Parser_A.java
+++ src/Parser_A.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class Parser_A {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a command: ");
+        String command = scanner.nextLine();
+        parseCommand(command);
+    }
+
+    public static void parseCommand(String command) {
+        String[] tokens = command.split(" ");
+        String operation = tokens[0];
+        int arg1 = Integer.parseInt(tokens[1]);
+        int arg2 = Integer.parseInt(tokens[2]);
+
+        switch (operation) {
+            case "add":
+                int sum = arg1 + arg2;
+                System.out.println("Result: " + sum);
+                break;
+            case "subtract":
+                int difference = arg1 - arg2;
+                System.out.println("Result: " + difference);
+                break;
+            default:
+                System.out.println("Invalid command.");
+                break;
+        }
+    }
 }
