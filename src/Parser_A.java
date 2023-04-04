1d179f47c12ca165ce692533f684eb9ff34d565e
Aayush Kafle <akafle1@unm.edu>
2023-04-04 09:04:02 -0600

    update parser and port

--- src/Parser_A.java
+++ src/Parser_A.java
@@ -1,33 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Parser_A {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a command: ");
-        String command = scanner.nextLine();
-        parseCommand(command);
+        while (scanner.hasNext()) {
+            String word = scanner.next();
+            parseWord(word);
+        }
+        scanner.close();
     }
 
-    public static void parseCommand(String command) {
-        String[] tokens = command.split(" ");
-        String operation = tokens[0];
-        int arg1 = Integer.parseInt(tokens[1]);
-        int arg2 = Integer.parseInt(tokens[2]);
-
-        switch (operation) {
+    public static String parseWord(String rawTextToClassify) {
+        switch (rawTextToClassify) {
             case "MOVE":
-                // AFTER this command I am thinking of add the ports so that whatever the port user is asking to take the
-                // value from we can retrieve that value. I am thinking of making a separate class which holds up, down,
-                // right, and left port values for all ports
-                int sum = arg1 + arg2;
-                System.out.println("Result: " + sum);
+                // call move interpreter
                 break;
             case "SWAP":
                 // add the function of swap command
                 break;
             default:
-                System.out.println("Invalid command.");
                 break;
         }
     }
--- src/Port_A.java
+++ src/Port_A.java
@@ -7,8 +7,12 @@ public class Port_A {
     private int rightPortAccValue;
     private int leftPortAccValue;
 
-    public Port_A(int siloIndex){
+    public Port_A(int siloIndex, int upPortAccValue, int downPortAccValue, int rightPortAccValue, int leftPortAccValue){
         // I am thinking of making the right value of silo index 0 to the left value of silo index 1
-
+        // this can be done whereever port class is called.
+        this.upPortAccValue = upPortAccValue;
+        this.downPortAccValue = downPortAccValue;
+        this.rightPortAccValue = rightPortAccValue;
+        this.leftPortAccValue = leftPortAccValue;
     }
 }
