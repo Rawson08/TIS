89e84e8d474d13dc51bd81a0eca355f06d51d81f
Aayush Kafle <akafle1@unm.edu>
2023-04-04 07:55:57 -0600

    added port (incomplete) for test

---
+++ src/Port_A.java
@@ -0,0 +1,14 @@
+/**
+ * This is the port has hold four side values for every silo
+ */
+public class Port_A {
+    private int upPortAccValue;
+    private int downPortAccValue;
+    private int rightPortAccValue;
+    private int leftPortAccValue;
+
+    public Port_A(int siloIndex){
+        // I am thinking of making the right value of silo index 0 to the left value of silo index 1
+
+    }
+}
