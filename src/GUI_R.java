d90ca62807ef4c906ce2e6af3ef0c354ffe24e30
Aayush Kafle <akafle1@unm.edu>
2023-04-18 13:43:23 -0600

    Makes Table for multiple input and multiple outputs.

--- src/GUI_R.java
+++ src/GUI_R.java
@@ -79,11 +79,17 @@ public class GUI_R extends Application {
         //TODO: Put inputLabel with inputArea in one VBox and same for output
 //        ioLabel.getChildren().addAll(inputLabel, outputLabel);
 //        ioPanel.getChildren().addAll(inputArea, outputArea);
-        String inputValueStr = interpreterA1.getInputValues().toString().replaceAll(",", "\n").replaceAll("[ \\[\\]]","");
-
-
         HBox vBox = new HBox();
-        vBox.getChildren().addAll(createInput(inputValueStr), createOutput(), createInput(inputValueStr), createOutput());
+        for (int i=0; i<interpreterA1.getInputValuesList().size(); i++){
+            System.out.println("list contains: " + interpreterA1.getInputValuesList().get(i));
+            String inputValueStr = interpreterA1.getInputValuesList().get(i).toString().replaceAll(",", "\n").replaceAll("[ \\[\\]]","");
+            vBox.getChildren().add(createInput(inputValueStr));
+        }
+        for (int j=0; j<interpreterA1.getOutputValuesList().size(); j++){
+            vBox.getChildren().add(createOutput());
+        }
+
+
         vBox.setPadding(new Insets(10));
         vBox.prefWidthProperty().bind(root.widthProperty().multiply(0.25));
 //        vBox.prefHeightProperty().bind(root.heightProperty().multiply(0.75));
