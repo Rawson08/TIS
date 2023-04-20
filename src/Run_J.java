a0995c59377bc90c222c680a6c61285420cb964e
Joel Gloetzner <jogloetzner@unm.edu>
2023-04-20 08:38:25 -0600

    updated run_J with offset sleep per silo

--- src/Run_J.java
+++ src/Run_J.java
@@ -1,11 +1,13 @@
 public class Run_J implements Runnable {
     private int i,j;
     private int index = 0;
+    private static int sleepTime = 1000;
 
     public Run_J(int i, int j) {
         this.i= i;
         this.j=j;
         this.index = index;
+        this.sleepTime = sleepTime+= 100;
     }
 
     public synchronized void run() {
@@ -20,7 +22,7 @@ public class Run_J implements Runnable {
                         }
                         if(index == length) index = 0;
                         try {
-                            Thread.sleep(1000); // pause the thread for 1 second
+                            Thread.sleep(sleepTime); // pause the thread for 1 second
                         } catch (InterruptedException e) {
                             // handle the exception if the thread is interrupted
                         }
